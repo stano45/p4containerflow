5fb2818ab8d6ad904dc1cd0805889474ee96e4e2
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-06-03 10:17:58 +0000

    Clean up, refactor

--- load_balancer/mycontroller.py
+++ load_balancer/mycontroller.py
@@ -2,7 +2,6 @@
 import argparse
 import os
 import sys
-from time import sleep
 
 import grpc
 
@@ -15,10 +14,8 @@ import p4runtime_lib.bmv2
 import p4runtime_lib.helper
 from p4runtime_lib.switch import ShutdownAllSwitchConnections
 
-SWITCH_TO_HOST_PORT = 1
-SWITCH_TO_SWITCH_PORT = 2
-
-def writeTableEntries(p4info_helper, sw):
+def writeTableEntriesS1(p4info_helper, sw):
+    # Group IP (for load balancing)
     table_entry = p4info_helper.buildTableEntry(
         table_name="MyIngress.ecmp_group",
         default_action=True,
@@ -40,139 +37,42 @@ def writeTableEntries(p4info_helper, sw):
     sw.WriteTableEntry(table_entry)
     print("Installed ecmp_group set_ecmp_select rule on %s" % sw.name)
     
-    table_entry = p4info_helper.buildTableEntry(
-    table_name="MyIngress.ecmp_nhop",
-    match_fields={
-        "meta.ecmp_select": 0
-    },
-    action_name="MyIngress.set_nhop",
-    action_params={
-        "nhop_dmac": "00:00:00:00:01:02",
-        "nhop_ipv4": "10.0.2.2",
-        "port": 2
-    })
-    sw.WriteTableEntry(table_entry)
-    print("Installed set_nhop rule for 10.0.2.2 on %s" % sw.name)
+    # Hops
+    updateEcmpNhopTable(p4info_helper=p4info_helper, sw=sw, ecmp_select=0, dmac="00:00:00:00:01:02", ipv4="10.0.2.2", port=2)
+    updateEcmpNhopTable(p4info_helper=p4info_helper, sw=sw, ecmp_select=1, dmac="00:00:00:00:01:03", ipv4="10.0.3.3", port=3)
     
-    table_entry = p4info_helper.buildTableEntry(
-    table_name="MyIngress.ecmp_nhop",
-    match_fields={
-        "meta.ecmp_select": 1
-    },
-    action_name="MyIngress.set_nhop",
-    action_params={
-        "nhop_dmac": "00:00:00:00:01:03",
-        "nhop_ipv4": "10.0.3.3",
-        "port": 3
-    })
-    sw.WriteTableEntry(table_entry)
-    print("Installed set_nhop rule for 10.0.3.3 on %s" % sw.name)
-    
-    table_entry = p4info_helper.buildTableEntry(
-    table_name="MyEgress.send_frame",
-    match_fields={
-        "standard_metadata.egress_port": 2
-    },
-    action_name="MyEgress.rewrite_mac",
-    action_params={
-        "smac": "00:00:00:01:02:00"
-    })
-    sw.WriteTableEntry(table_entry)
-    print("Installed send_frame rewrite_mac rule for port 2 on %s" % sw.name)
-    
+    # Egress
+    updateSendFrameTable(p4info_helper=p4info_helper, sw=sw, egress_port=2, smac="00:00:00:01:02:00")
+    updateSendFrameTable(p4info_helper=p4info_helper, sw=sw, egress_port=3, smac="00:00:00:01:03:00")
+
+
+def updateSendFrameTable(p4info_helper, sw, egress_port, smac):
     table_entry = p4info_helper.buildTableEntry(
         table_name="MyEgress.send_frame",
         match_fields={
-            "standard_metadata.egress_port": 3
+            "standard_metadata.egress_port": egress_port
         },
         action_name="MyEgress.rewrite_mac",
         action_params={
-            "smac": "00:00:00:01:03:00"
+            "smac": smac
         })
     sw.WriteTableEntry(table_entry)
-    print("Installed send_frame rewrite_mac rule for port 3 on %s" % sw.name)
+    print(f"Updated the 'send_frame' table on {sw.name=} with {egress_port=}, {smac=}")
 
-
-def writeTunnelRules(p4info_helper, ingress_sw, egress_sw, tunnel_id,
-                     dst_eth_addr, dst_ip_addr):
-    """
-    Installs three rules:
-    1) An tunnel ingress rule on the ingress switch in the ipv4_lpm table that
-       encapsulates traffic into a tunnel with the specified ID
-    2) A transit rule on the ingress switch that forwards traffic based on
-       the specified ID
-    3) An tunnel egress rule on the egress switch that decapsulates traffic
-       with the specified ID and sends it to the host
-
-    :param p4info_helper: the P4Info helper
-    :param ingress_sw: the ingress switch connection
-    :param egress_sw: the egress switch connection
-    :param tunnel_id: the specified tunnel ID
-    :param dst_eth_addr: the destination IP to match in the ingress rule
-    :param dst_ip_addr: the destination Ethernet address to write in the
-                        egress rule
-    """
-    # 1) Tunnel Ingress Rule
+def updateEcmpNhopTable(p4info_helper, sw, ecmp_select, dmac, ipv4, port):
     table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.ipv4_lpm",
+        table_name="MyIngress.ecmp_nhop",
         match_fields={
-            "hdr.ipv4.dstAddr": (dst_ip_addr, 32)
+            "meta.ecmp_select": ecmp_select
         },
-        action_name="MyIngress.myTunnel_ingress",
+        action_name="MyIngress.set_nhop",
         action_params={
-            "dst_id": tunnel_id,
+            "nhop_dmac": dmac,
+            "nhop_ipv4": ipv4,
+            "port": port
         })
-    ingress_sw.WriteTableEntry(table_entry)
-    print("Installed ingress tunnel rule on %s" % ingress_sw.name)
-
-    # 2) Tunnel Transit Rule
-    # The rule will need to be added to the myTunnel_exact table and match on
-    # the tunnel ID (hdr.myTunnel.dst_id). Traffic will need to be forwarded
-    # using the myTunnel_forward action on the port connected to the next switch.
-    #
-    # For our simple topology, switch 1 and switch 2 are connected using a
-    # link attached to port 2 on both switches. We have defined a variable at
-    # the top of the file, SWITCH_TO_SWITCH_PORT, that you can use as the output
-    # port for this action.
-    #
-    # We will only need a transit rule on the ingress switch because we are
-    # using a simple topology. In general, you'll need on transit rule for
-    # each switch in the path (except the last switch, which has the egress rule),
-    # and you will need to select the port dynamically for each switch based on
-    # your topology.
-
-    # TODO build the transit rule
-    # TODO install the transit rule on the ingress switch
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_forward",
-        action_params={
-            "port": SWITCH_TO_SWITCH_PORT
-        })
-    ingress_sw.WriteTableEntry(table_entry)
-    print("Installed transit tunnel rule on %s" % ingress_sw.name)
-
-    # 3) Tunnel Egress Rule
-    # For our simple topology, the host will always be located on the
-    # SWITCH_TO_HOST_PORT (port 1).
-    # In general, you will need to keep track of which port the host is
-    # connected to.
-    table_entry = p4info_helper.buildTableEntry(
-        table_name="MyIngress.myTunnel_exact",
-        match_fields={
-            "hdr.myTunnel.dst_id": tunnel_id
-        },
-        action_name="MyIngress.myTunnel_egress",
-        action_params={
-            "dstAddr": dst_eth_addr,
-            "port": SWITCH_TO_HOST_PORT
-        })
-    egress_sw.WriteTableEntry(table_entry)
-    print("Installed egress tunnel rule on %s" % egress_sw.name)
-
+    sw.WriteTableEntry(table_entry)
+    print(f"Updated the 'ecmp_nhop' table on {sw.name=} with {ecmp_select=}, {ipv4=}, {dmac=}, {port=}")
 
 def readTableRules(p4info_helper, sw):
     """
@@ -200,25 +100,6 @@ def readTableRules(p4info_helper, sw):
                 print('%r' % p.value, end=' ')
             print()
 
-def printCounter(p4info_helper, sw, counter_name, index):
-    """
-    Reads the specified counter at the specified index from the switch. In our
-    program, the index is the tunnel ID. If the index is 0, it will return all
-    values from the counter.
-
-    :param p4info_helper: the P4Info helper
-    :param sw:  the switch connection
-    :param counter_name: the name of the counter from the P4 program
-    :param index: the counter index (in our case, the tunnel ID)
-    """
-    for response in sw.ReadCounters(p4info_helper.get_counters_id(counter_name), index):
-        for entity in response.entities:
-            counter = entity.counter_entry
-            print("%s %s %d: %d packets (%d bytes)" % (
-                sw.name, counter_name, index,
-                counter.data.packet_count, counter.data.byte_count
-            ))
-
 def printGrpcError(e):
     print("gRPC Error:", e.details(), end=' ')
     status_code = e.code()
@@ -239,11 +120,6 @@ def main(p4info_file_path, bmv2_file_path):
             address='127.0.0.1:50051',
             device_id=0,
             proto_dump_file='logs/s1-p4runtime-requests.txt')
-        # s2 = p4runtime_lib.bmv2.Bmv2SwitchConnection(
-        #     name='s2',
-        #     address='127.0.0.1:50052',
-        #     device_id=1,
-        #     proto_dump_file='logs/s2-p4runtime-requests.txt')
 
         # Send master arbitration update message to establish this controller as
         # master (required by P4Runtime before performing any other write operation)
@@ -254,31 +130,9 @@ def main(p4info_file_path, bmv2_file_path):
         s1.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
                                        bmv2_json_file_path=bmv2_file_path)
         print("Installed P4 Program using SetForwardingPipelineConfig on s1")
-        # s2.SetForwardingPipelineConfig(p4info=p4info_helper.p4info,
-        #                                bmv2_json_file_path=bmv2_file_path)
-        # print("Installed P4 Program using SetForwardingPipelineConfig on s2")
 
-        writeTableEntries(p4info_helper, sw=s1)
-        # Write the rules that tunnel traffic from h1 to h2
-        # writeTunnelRules(p4info_helper, ingress_sw=s1, egress_sw=s2, tunnel_id=100,
-        #                  dst_eth_addr="08:00:00:00:02:22", dst_ip_addr="10.0.2.2")
-
-        # # Write the rules that tunnel traffic from h2 to h1
-        # writeTunnelRules(p4info_helper, ingress_sw=s2, egress_sw=s1, tunnel_id=200,
-        #                  dst_eth_addr="08:00:00:00:01:11", dst_ip_addr="10.0.1.1")
-
-        # TODO Uncomment the following two lines to read table entries from s1 and s2
+        writeTableEntriesS1(p4info_helper, sw=s1)
         readTableRules(p4info_helper, s1)
-        # readTableRules(p4info_helper, s2)
-
-        # Print the tunnel counters every 2 seconds
-        # while True:
-        #     sleep(2)
-        #     print('\n----- Reading tunnel counters -----')
-        #     # printCounter(p4info_helper, s1, "MyIngress.ingressTunnelCounter", 100)
-        #     # printCounter(p4info_helper, s2, "MyIngress.egressTunnelCounter", 100)
-        #     # printCounter(p4info_helper, s2, "MyIngress.ingressTunnelCounter", 200)
-        #     # printCounter(p4info_helper, s1, "MyIngress.egressTunnelCounter", 200)
 
     except KeyboardInterrupt:
         print(" Shutting down.")
