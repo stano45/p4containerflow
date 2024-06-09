206f3bc7de2b570f33de408bb1e4a05d359a608a
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-06-09 11:35:43 +0000

    Fix paths, add client and server script

--- scripts/2_edit_img.sh
+++ scripts/2_edit_img.sh
@@ -2,4 +2,4 @@
 
 sudo chown -R p4:p4 /home/p4/images
 
-python3 /home/p4/p4containerflow/tcp/edit_files_img.py /home/p4/images/files.img "10.0.3.3" "10.0.4.4"
+python3 /home/p4/p4containerflow/scripts/edit_files_img.py /home/p4/images/files.img "10.0.3.3" "10.0.4.4"
---
+++ scripts/server.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/p4/p4containerflow/tcp/server 12345
