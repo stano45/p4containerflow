7dfd05b8e884e73a4c85d5bcab1c483e74a4093d
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-06-15 22:11:34 +0000

    Working with isolated subnets

--- scripts/build_network_podman.sh
+++ scripts/build_network_podman.sh
@@ -10,21 +10,21 @@ sudo ip link set dev s1-eth3 address 00:00:00:01:03:00
 sudo ip link set dev s3-eth2 address 00:00:00:03:02:00
 
 
-sudo podman network create --interface-name s1-eth1 --subnet 10.1.1.0/24 --gateway 10.1.1.10  h1-net
+sudo podman network create -o isolate --interface-name s1-eth1 --subnet 10.1.1.0/24 --gateway 10.1.1.10 h1-net
 sudo podman pod create --name h1-pod --network h1-net:ip=10.1.1.1 --mac-address 08:00:00:00:01:01
 # sudo podman run -d --name iperf-server1 --pod h1-pod docker.io/networkstatic/iperf3 -s
 sudo podman run -d --name iperf-server1 --pod h1-pod docker.io/subfuzion/netcat -vl 12345
 sudo ip link set dev s1-eth1 address 00:00:00:01:01:00
 # sudo arp -i veth0 -s 10.1.1.10 00:00:00:01:01:00
 
-sudo podman network create --interface-name s2-eth1 --subnet 10.2.2.0/24 --gateway 10.2.2.20  h2-net
+sudo podman network create --interface-name s2-eth1 --subnet 10.2.2.0/24 --gateway 10.2.2.20 h2-net
 sudo podman pod create --name h2-pod --network h2-net:ip=10.2.2.2 --mac-address 08:00:00:00:02:02
 #sudo podman run -d --name iperf-server2 --pod h2-pod docker.io/networkstatic/iperf3 -s
 sudo podman run -d --name iperf-server2 --pod h2-pod docker.io/subfuzion/netcat -vl 12345
 sudo ip link set dev s2-eth1 address 00:00:00:02:01:00
 # sudo arp -i veth1 -s 10.2.2.20 00:00:00:02:02:00
 
-sudo podman network create --interface-name s3-eth1 --subnet 10.3.3.0/24 --gateway 10.3.3.30  h3-net
+sudo podman network create -o isolate --interface-name s3-eth1 --subnet 10.3.3.0/24 --gateway 10.3.3.30 h3-net
 sudo podman pod create --name h3-pod --network h3-net:ip=10.3.3.3 --mac-address 08:00:00:00:03:03
 # sudo podman run -d --name iperf-server3 --pod h3-pod docker.io/networkstatic/iperf3 -s
 sudo podman run -d --name iperf-server3 --pod h3-pod docker.io/subfuzion/netcat -vl 12345
@@ -32,7 +32,7 @@ sudo ip link set dev s3-eth1 address 00:00:00:03:01:00
 # sudo arp -i veth2 -s 10.3.3.30 00:00:00:03:03:00
 
 
-sudo podman network create --interface-name s4-eth1 --subnet 10.4.4.0/24 --gateway 10.4.4.40  h4-net
+sudo podman network create -o isolate --interface-name s4-eth1 --subnet 10.4.4.0/24 --gateway 10.4.4.40 h4-net
 sudo podman pod create --name h4-pod --network h4-net:ip=10.4.4.4 --mac-address 08:00:00:00:04:04
 # sudo podman run -d --name iperf-server4 --pod h4-pod docker.io/networkstatic/iperf3 -s
 sudo podman run -d --name iperf-server4 --pod h4-pod docker.io/subfuzion/netcat -vl 12345
--- scripts/destroy_network_podman.sh
+++ scripts/destroy_network_podman.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
 
+sudo podman container kill iperf-server1
 sudo podman rm -f iperf-server1
+sudo podman container kill iperf-client
 sudo podman rm -f iperf-client
 sudo podman pod rm -f h1-pod
 sudo podman network rm -f h1-net
 
+sudo podman container kill iperf-server2
 sudo podman rm -f iperf-server2
 sudo podman pod rm -f h2-pod
 sudo podman network rm -f h2-net
 
+sudo podman container kill iperf-server3
 sudo podman rm -f iperf-server3
 sudo podman pod rm -f h3-pod
 sudo podman network rm -f h3-net
 
+sudo podman container kill iperf-server4
 sudo podman rm -f iperf-server4
 sudo podman pod rm -f h4-pod
 sudo podman network rm -f h4-net
