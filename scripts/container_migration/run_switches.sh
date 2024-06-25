a547efe2e201fd4825471b515247c82163587bd4
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-06-25 21:05:52 +0000

    Change switch subnet to 10.1.0.1\24

--- scripts/container_migration/run_switches.sh
+++ scripts/container_migration/run_switches.sh
@@ -1,11 +1,10 @@
-sudo podman network create s1-net --subnet 10.0.0.0/24
+sudo podman network create s1-net --subnet 10.1.0.0/24 --gateway 10.1.0.10
 
 sudo podman run -d \
     --name s1 \
     --privileged \
     --replace \
-    --network s1-net \
-    --ip 10.0.0.0 \
+    --network s1-net:ip=10.1.0.1 \
     -v /home/p4/p4containerflow/scripts/container_migration/s1.sh:/s1.sh \
     -v /home/p4/p4containerflow/load_balancer/build/load_balance.json:/load_balance.json \
     --entrypoint /s1.sh \
