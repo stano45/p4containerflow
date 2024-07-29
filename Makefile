fabb2a96b1cf95607ec99ed53ace9163b6c9866a
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-07-29 17:23:00 +0200

    Update migration script (#13)
    * Update migration script
    
    * Dump image on addr rewrite fail, set netcat & iperf to use ipv4
    
    * Format py

--- Makefile
+++ Makefile
@@ -1,3 +1,6 @@
+SHELL := /bin/bash
+MIGRATION_SCRIPT_PATH := ./scripts/switch_container/cr.sh
+
 all: compile net control
 
 net: 
@@ -9,6 +12,13 @@ compile:
 control:
 	cd controller && sleep 2 && make
 
+migrate:
+	@if [ "${SOURCE}" = "" ] || [ "${TARGET}" = "" ]; then \
+		echo "Usage: make migrate SOURCE=x TARGET=y"; \
+	else \
+		$(MIGRATION_SCRIPT_PATH) ${SOURCE} ${TARGET}; \
+	fi
+
 clean:
 	cd scripts/switch_container && make clean
 
@@ -19,7 +29,7 @@ tcp-client:
 	sudo podman run -it --rm --replace --name tcp-client --pod h1-pod tcp-client
 
 netcat-client:
-	sudo podman run -it --rm --replace --name netcat-client --pod h1-pod docker.io/gophernet/netcat -v 10.1.1.10 12345
+	sudo podman run -it --rm --replace --name netcat-client --pod h1-pod docker.io/subfuzion/netcat -4 -v 10.1.1.10 12345
 
 iperf-client:
-	sudo podman run -it --rm --replace --name iperf-client --pod h1-pod docker.io/networkstatic/iperf3 -c 10.1.1.10 -p 12345 -t 30
+	sudo podman run -it --rm --replace --name iperf-client --pod h1-pod docker.io/networkstatic/iperf3 -4 -c 10.1.1.10 -p 12345 -t 30
--- scripts/switch_container/cr.sh
+++ scripts/switch_container/cr.sh
@@ -1,26 +1,47 @@
+#!/bin/bash
+
+# Check if exactly two arguments are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <source_idx> <target_idx>"
+    exit 1
+fi
+
+# Assigning arguments to variables
+SOURCE_IDX=$1
+TARGET_IDX=$2
+
 CHECKPOINT_DIR=/tmp/checkpoints
 CHECKPOINT_PATH=$CHECKPOINT_DIR/checkpoint.tar
 
-SOURCE_IDX=3
 SOURCE_HOST=h$SOURCE_IDX
 SOURCE_IP=10.$SOURCE_IDX.$SOURCE_IDX.$SOURCE_IDX
 
-TARGET_IDX=4
 TARGET_HOST=h$TARGET_IDX
 TARGET_IP=10.$TARGET_IDX.$TARGET_IDX.$TARGET_IDX
 TARGET_MAC=08:00:00:00:0$TARGET_IDX:0$TARGET_IDX
 
+# Creating checkpoint directory
 sudo mkdir -p $CHECKPOINT_DIR
 
+# Checkpoint the source container
 sudo podman container checkpoint --export $CHECKPOINT_PATH --compress none --keep --tcp-established $SOURCE_HOST
 sudo podman rm -f $SOURCE_HOST
 
+# Edit the checkpoint files with new IP
 sudo /home/p4/p4containerflow/scripts/edit_files_img.py $CHECKPOINT_PATH $SOURCE_IP $TARGET_IP
 
+# Kill and remove the target container
+sudo podman container kill ${TARGET_HOST}
+sudo podman container rm -f ${TARGET_HOST}
+
+# Restore the container with new settings
 sudo podman container restore --import $CHECKPOINT_PATH --keep --tcp-established --ignore-static-ip --ignore-static-mac --pod ${TARGET_HOST}-pod
+
+# Rename the restored container
 # --name cannot be used with --tcp-established on restore
 sudo podman rename $SOURCE_HOST $TARGET_HOST
 
+# Update the node information
 curl -X POST http://127.0.0.1:5000/update_node \
     -H "Content-Type: application/json" \
     -d "{\"old_ipv4\":\"${SOURCE_IP}\", \"new_ipv4\":\"${TARGET_IP}\", \"dmac\":\"${TARGET_MAC}\", \"eport\":\"${TARGET_IDX}\"}"
