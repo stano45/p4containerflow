66a10758067c21c04a9f02990a1a5c85a2d1e84d
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-08-10 20:11:22 +0200

    Move load balancer node configuration to switches.json (#17)
    * Use a json config as argument for controller
    
    Previously, the p4info, bmv2_json and
    multi_switch arguments were passed to the
    controller as command line arguments and the
    switch configuration was hardcoded. This commit
    makes it possible to configure any number of
    switches in the configuration file, and also to
    customize the parameters of individual switches.
    
    * Format all json runtime files
    
    * Add comment to the controller Makefile target
    
    * Add the lb_nodes field to the switches JSON config
    
    The lb_nodes field is a list of the nodes that
    the load balancer routes traffic to. This field
    consists of a list of dictionaries, where each
    dictionary contains the following fields:
    - ip: the IP address of the node
    - mac: the MAC address of the node
    - port: the switch port to use to route traffic
      to the node
    The controller uses this field to program the
    load balancer switch with proper match-action
    table entries.
    The controller tracks state of the table entries
    to allow updates after a container migration.

--- controller/node_manager.py
+++ controller/node_manager.py
@@ -2,13 +2,29 @@ from switch_controller import SwitchController
 
 
 class NodeManager(object):
-    def __init__(self, switch_controller: SwitchController):
+    def __init__(self, switch_controller: SwitchController, lb_nodes):
         self.switch_controller = switch_controller
         # ipv4 -> ecmp_select_id
-        self.node_map = {
-            "10.0.2.2": 1,
-            "10.0.3.3": 2,
-        }
+        self.node_map = {}
+        for i, node in enumerate(lb_nodes):
+            # 0 is reserved for 10.0.1.1 (client), the rest for nodes
+            ip = node["ip"]
+            mac = node["mac"]
+            port = node["port"]
+            self.node_map[ip] = i + 1
+            self.switch_controller.insertEcmpNhopEntry(
+                ecmp_select=i + 1,
+                dmac=mac,
+                ipv4=ip,
+                port=port,
+                update_type="INSERT",
+            )
+        self.switch_controller.insertEcmpGroupEntry(
+            matchDstAddr=["10.0.1.10", 32],
+            ecmp_base=1,
+            ecmp_count=len(lb_nodes),
+        )
+        self.switch_controller.readTableRules()
 
     def updateNode(self, old_ip, new_ip, dest_mac, egress_port):
         if new_ip in self.node_map:
@@ -19,7 +35,7 @@ class NodeManager(object):
 
         ecmp_select_id = self.node_map.pop(old_ip)
 
-        self.switch_controller.upsertEcmpNhopEntry(
+        self.switch_controller.insertEcmpNhopEntry(
             ecmp_select=ecmp_select_id,
             dmac=dest_mac,
             ipv4=new_ip,
