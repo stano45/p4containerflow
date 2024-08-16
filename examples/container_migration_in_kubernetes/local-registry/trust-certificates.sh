ae53518d5829c3d7dff5e729614ff80ee918fffd
Radostin Stoyanov <rstoyanov@fedoraproject.org>
2024-08-16 09:57:28 +0100

    local-registry: enable support for ubuntu/fedora
    Installing root CA certificate requires different commands on different
    Linux distributions. This patch updates the script to support Ubuntu
    in addition to Fedora/RHEL.
    
    Signed-off-by: Radostin Stoyanov <rstoyanov@fedoraproject.org>

--- examples/container_migration_in_kubernetes/local-registry/trust-certificates.sh
+++ examples/container_migration_in_kubernetes/local-registry/trust-certificates.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
 
-sudo mkdir -p /etc/pki/ca-trust/source/anchors/
+if [ -f /etc/os-release ]; then
+	. /etc/os-release
+else
+	echo "Cannot find /etc/os-release"
+	exit 1
+fi
 
-sudo cp ./certs/container-registry-local.crt /etc/pki/ca-trust/source/anchors/
-sudo update-ca-trust
-sudo trust list
+case "$ID" in
+	fedora|rhel)
+		sudo mkdir -p /etc/pki/ca-trust/source/anchors/
+		sudo cp ./certs/container-registry-local.crt /etc/pki/ca-trust/source/anchors/
+		sudo update-ca-trust
+		;;
+
+	ubuntu)
+		sudo mkdir -p /usr/local/share/ca-certificates
+		sudo cp ./certs/container-registry-local.crt /usr/local/share/ca-certificates/
+		sudo update-ca-certificates
+		;;
+
+	*)
+		echo "Unsupported or unrecognized distribution: $ID"
+		exit 1
+		;;
+esac
+
+exit 0
