14fa955d4c0aad4b81a74102d5fce33d7142ec74
Stanislav Kosorin <s.kosorin@campus.tu-berlin.de>
2024-09-02 21:59:08 +0200

    Working client & controller images

--- examples/container_migration_in_kubernetes/rst.sh
+++ examples/container_migration_in_kubernetes/rst.sh
@@ -1,3 +1,4 @@
+sudo sysctl -w net.ipv4.ip_forward=1
 sudo kubeadm reset -f
 sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --cri-socket=unix:///var/run/crio/crio.sock
 
@@ -9,5 +10,9 @@ kubectl taint nodes --all node-role.kubernetes.io/master-
 kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
 kubectl apply -f manifests/kube-router-daemonset.yaml
+
+sleep 5
+kubectl apply -f manifests/http-server-deployment.yaml
+kubectl apply -f manifests/http-client-deployment.yaml
 
 kubectl get pods --all-namespaces
