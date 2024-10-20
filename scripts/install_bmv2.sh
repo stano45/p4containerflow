00087c052f0f197f8da58c8077d5ffb4ed50bfc5
Stanislav Kosorin <stanokosorin4@gmail.com>
2024-10-20 13:40:56 +0200

    Update install script
    Signed-off-by: Stanislav Kosorin <stanokosorin4@gmail.com>

--- scripts/install_bmv2.sh
+++ scripts/install_bmv2.sh
@@ -2,22 +2,36 @@
 # Install the p4 compiler on Ubuntu 22.04+
 
 git clone https://github.com/p4lang/behavioral-model.git
-sudo apt-get install -y automake cmake libgmp-dev \
-    libpcap-dev libboost-dev libboost-test-dev libboost-program-options-dev \
-    libboost-system-dev libboost-filesystem-dev libboost-thread-dev \
-    libevent-dev libtool flex bison pkg-config g++ libssl-dev
+sudo apt-get install -y \
+    automake \
+    cmake \
+    libgmp-dev \
+    libpcap-dev \
+    libboost-dev \
+    libboost-test-dev \
+    libboost-program-options-dev \
+    libboost-system-dev \
+    libboost-filesystem-dev \
+    libboost-thread-dev \
+    libevent-dev \
+    libtool \
+    flex \
+    bison \
+    pkg-config \
+    g++ \
+    libssl-dev
 
 cd behavioral-model
 cd ci
 chmod +x install-nanomsg.sh
-chmod +x install-thrift.sh
 ./install-nanomsg.sh
-./install-thrift.sh
+
 python3 -m pip install thrift==0.21.0
 cd ..
 ./autogen.sh
-./configure
+./configure --with-pi --with-thrift
 make -j$(nproc)
-sudo make install  # if you need to install bmv2
+sudo make install-strip # if you need to install bmv2
+sudo ldconfig
 cd ..
 # rm -rf behavioral-model
